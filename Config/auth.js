fb564e40cb4f2706981501112e95edf52d2a69d2
EdelCisco <edelalmeida84gmail.com>
2025-08-05 13:32:20 +0200

    test

--- Config/auth.js
+++ Config/auth.js
@@ -9,7 +9,7 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://hope-backend-production-813f.up.railway.app/auth/google/callback",
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
